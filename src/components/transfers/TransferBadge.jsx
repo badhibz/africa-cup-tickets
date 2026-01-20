fde2ffb61f4318133ce9fa3c6fb208fc77b6d054
Hiba Khchichi <khchichi.hiba@gmail.com>
2026-01-20 21:22:00 +0000

    File changes

--- src/components/transfers/TransferBadge.jsx
+++ src/components/transfers/TransferBadge.jsx
@@ -10,15 +10,15 @@ export default function TransferBadge({ count, type }) {
       </div>
 
       {/* Badge Icon - Bottom Right */}
-      <div className={`absolute -bottom-2 -right-2 w-6 h-6 bg-white border-2 rounded-full flex items-center justify-center ${
+      <div className={`absolute -bottom-2 -right-2 w-6 h-6 bg-transparent border-2 rounded-full flex items-center justify-center ${
         type === 'sent' 
-          ? 'border-[#718096]' 
-          : 'border-[#8E9B8E]'
+          ? 'border-[#5B8DEE]' 
+          : 'border-[#52B69A]'
       }`}>
         {type === 'sent' ? (
-          <ArrowUpRight className="w-3.5 h-3.5" style={{ color: '#718096' }} />
+          <ArrowUpRight className="w-3.5 h-3.5" style={{ color: '#5B8DEE' }} />
         ) : (
-          <ArrowDownLeft className="w-3.5 h-3.5" style={{ color: '#8E9B8E' }} />
+          <ArrowDownLeft className="w-3.5 h-3.5" style={{ color: '#52B69A' }} />
         )}
       </div>
     </div>
